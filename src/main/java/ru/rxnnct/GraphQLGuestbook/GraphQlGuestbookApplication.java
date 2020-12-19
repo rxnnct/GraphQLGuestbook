48e217d8b2d92e74b9eaec2ef8efed81c8d1c3fd
Rixen <rixennoct@rambler.ru>
2020-12-19 22:11:58 +0300

    Initial commit

---
+++ src/main/java/ru/rxnnct/GraphQLGuestbook/GraphQlGuestbookApplication.java
@@ -0,0 +1,13 @@
+package ru.rxnnct.GraphQLGuestbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlGuestbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlGuestbookApplication.class, args);
+	}
+
+}
